b31fea2255f5da4fe3e7b4decb2882fed3c30884
Adrian Jose Gomez Torres <adrianjgt@gmail.com>
2022-06-03 19:56:21 -0300

    Start the creation of the website
    Roberto has been an amazing teach er on this course
    and I am very greatful of meeting him.

---
+++ src/lib/HeroSection.svelte
@@ -0,0 +1,52 @@
+<script>
+
+</script>
+
+<main>
+  <div class="first-row"> 
+    <div class="tagline">
+      <h1>
+        <span>Find yourself in the world</span> 
+        The webapp to follow your many journies
+      </h1>
+    </div>
+   <img id="logo" src="src/assets/countries-app-logo.png" alt="imagen del mundo">
+  </div>
+  
+</main>
+
+<style>
+ 
+
+  .first-row{
+    margin-top: 30px;
+    display:flex;
+    height:364px;
+    align-items:center;
+    justify-content: space-around;
+  }
+
+  h1 {
+    width: 600px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 900;
+    font-size: 60px;
+    color: #00BBB6;
+  }
+
+  span {
+    font-family: 'Josefin Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    color: #FC8362;
+    display: block;
+  }
+
+  #logo {
+    width: 430px;
+    height: 364px;
+  }
+
+</style>
---
+++ src/lib/SwitchTab.svelte
@@ -0,0 +1,85 @@
+<script>
+const option1="Countries you want to visit";
+const option2="Countries you have been";
+let optionChange;
+let optionState = [true, false]; 
+
+function clicked(){
+    if(optionState[0] == true){
+        optionState = [false, true]
+    } else{
+        optionState = [true, false]
+    };
+
+    console.log("hello adrian", optionState);
+};
+
+
+</script>
+
+
+<div on:click={clicked} class="box-grey" class:inactive = {optionState[0]==false}>
+    <div class="box-green" class:move-block = {optionState[0]==false}/>
+    <div class="options">
+        <div class="option one" class:inactive = {optionState[0]==false}>{option1}</div>
+        <div class="option two" class:inactive = {optionState[1]==false}>{option2}</div>
+    </div>
+</div>
+
+
+
+
+<style>
+    .box-grey{
+        display:flex;
+        align-items: center;
+        height: 86px;
+        width: 450px;
+        background-color: #ccc;
+        border-radius:20px;
+        justify-content: flex-start;
+        padding:0 10px;
+        
+    }
+
+
+    .box-green{
+        background-color: #00BBB6;
+        width: 225px;
+        height:70px;
+        border-radius:15px;
+        transition: all ease-in-out 500ms;
+        
+    }
+
+    .options{
+        position: absolute;
+        height: 86px;
+        width: 450px;
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .option{
+        color: white;
+        font-family: 'Josefin Sans';
+        text-align: center;
+        width:225px;
+    }
+
+    .inactive{
+        color:rgba(0, 0, 0, 0.2);
+        transition: all ease-in-out 500ms;
+    }
+    
+    .move-block{
+        transform: translateX(100%);        
+    }
+
+
+
+   
+
+
+</style>
